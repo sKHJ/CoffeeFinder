252d096296db36096423513fe9ab7ea4723a2ea5
sKHJ <gkswnswnwkr@gmail.com>
2017-05-26 16:34:00 +0900

    ver1

---
+++ SampleLocationMapOverlay/app/src/main/java/org/androidtown/location/EasySearch1.java
@@ -0,0 +1,25 @@
+package org.androidtown.location;
+
+/**
+ * Created by Administrator on 2017-05-16.
+ */
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v7.app.ActionBarActivity;
+        import android.widget.TextView;
+
+/**
+ * Created by songmho on 2015-01-18.
+ */
+public class EasySearch1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.easysearch);
+
+
+    }
+}
